71f3cca7b3e6d2248bd4924d6bfebd2fd5e37894
Jon Wire <iambipedal@gmail.com>
2021-05-02 13:23:28 -0500

    attempting hello world via nat-pmp

--- p-wing/lib/index.js
+++ p-wing/lib/index.js
@@ -1,23 +1,38 @@
+const express = require('express');
 const natpmp = require('nat-pmp');
 
 module.exports = async () => {
 	console.log('starting ...');
 	try {
-		// var client = natpmp.connect('192.168.1.1');
-		var client = natpmp.connect('100.115.92.207');
+		let ip;
+
+		var client = natpmp.connect('192.168.1.1');
+		// var client = natpmp.connect('100.115.92.207');
 
 		client.externalIp(function (err, info) {
 			if (err) throw err;
-			console.log('Current external IP address: %s', info.ip.join('.'));
+			ip = info.ip.join('.');
+			console.log(`Current external IP address: ${ip}`);
 		});
 
+		const port = 6789;
+
 		client.portMapping({
-			private: 22,
-			public: 2222,
+			private: port,
+			public: port,
 			ttl: 3600
 		}, function (err, info) {
 			if (err) throw err;
 			console.log(info);
+
+			const app = express();
+			app.get('/', (request, response) => {
+				response.send('Hello world');
+			});
+
+			app.listen();
+			console.log(`listening on ${ip}:${port} ... `);
+
 		});
 	} catch (err) {
 		console.log('ERROR', err);
